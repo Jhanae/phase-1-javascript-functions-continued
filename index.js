0d4671728e29863dc5189744d2ff539cc8d100b8
Jhanae <jhanaechisholm12@gmail.com>
2021-05-18 18:16:09 -0400

    commit tree 955894bd4fe027141f6c4f1374aaf70fda717d6e

--- index.js
+++ index.js
@@ -1,1 +1,28 @@
 // Your code here
+function saturdayFun()
+{
+    return 'This Saturday, I want to roller-skate!'
+}
+
+function saturdayFun(sleep)
+{
+    if(sleep === undefined)
+    {
+      return 'This Saturday, I want to roller-skate!';
+    }
+    else
+    return 'This Saturday, I want to ' + sleep + '!';
+}
+const mondayWork = function(arg = 'go to the office'){
+    return 'This Monday, I will ' + arg + '.';
+}
+function wrapAdjective(des)
+{
+    if(des === undefined) des = '*';
+    return function(name){
+        if(name === undefined)
+        name="special";
+        return 'You are ' + des + name + des + '!';
+    }
+}
+const encouragingPromptFunction = wrapAdjective("!!!");
